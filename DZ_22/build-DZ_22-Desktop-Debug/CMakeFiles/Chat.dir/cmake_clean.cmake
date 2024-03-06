b15aa8d90a7fd9e36f4cf971ba288e2c3927941e
SashaKo <a.kopovaya@yandex.ru>
2024-03-06 18:54:58 +0300

    cross-platform chat

---
+++ DZ_22/build-DZ_22-Desktop-Debug/CMakeFiles/Chat.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat.dir/Chat.cpp.o"
+  "CMakeFiles/Chat.dir/Chat.cpp.o.d"
+  "CMakeFiles/Chat.dir/Message.cpp.o"
+  "CMakeFiles/Chat.dir/Message.cpp.o.d"
+  "CMakeFiles/Chat.dir/User.cpp.o"
+  "CMakeFiles/Chat.dir/User.cpp.o.d"
+  "CMakeFiles/Chat.dir/main.cpp.o"
+  "CMakeFiles/Chat.dir/main.cpp.o.d"
+  "Chat"
+  "Chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
